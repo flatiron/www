0fffc1185ae8eb5816fbce3b5ab749b7fc168244
Paolo Fragomeni <hij1nx@me.com>
2012-01-15 23:14:17 -0500

    rewrite

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,22 @@
-
 $(function() {
-  $("#nav a, #diagram a").click(function(event) {
-    $("#scrolling-content").animate({"scrollTop": ($(this.hash)[0].offsetTop - 200) }, 400);
+
+  var toc = $('#toc-content');
+
+  $(document).scroll(function() {
+
+    if ($(document).scrollTop() >= 247) {
+      toc.addClass('fixed-toc');
+    }
+    else {
+      toc.removeClass('fixed-toc');
+    }
+  });
+
+
+  $('[href="#home"]').click(function() {
+    $('html, body').animate({"scrollTop": 0 }, 400);
     return false;
   });
+
+
 });
